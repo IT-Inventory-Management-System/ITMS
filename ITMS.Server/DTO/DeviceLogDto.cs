d892c8ede28b3d6c03a97222b10fc38e2dd0a7d4
divyanshrampal28 <divyansh.rampal@cginfinity.com>
2023-12-27 14:03:12 +0530

    "Frontend"

--- ITMS.Server/DTO/DeviceLogDto.cs
+++ ITMS.Server/DTO/DeviceLogDto.cs
@@ -1,23 +1,23 @@
-﻿//// DTOs/DeviceLogDto.cs
-//public class DeviceLogDto
-//{
-//    public Guid Id { get; set; }
-//    public Guid DeviceId { get; set; }
-//    public Guid ModelId { get; set; }
-//    public string UserId { get; set; }
-//    public string Description { get; set; }
-//    public Guid StatusId { get; set; }
-//    public DateTime AllotedDate { get; set; }
-//    public string Action { get; set; }
-//}
+﻿// DTOs/DeviceLogDto.cs
+public class DeviceLogDto
+{
+    public Guid Id { get; set; }
+    public Guid DeviceId { get; set; }
+    public Guid ModelId { get; set; }
+    public string UserId { get; set; }
+    public string Description { get; set; }
+    public Guid StatusId { get; set; }
+    public DateTime AllotedDate { get; set; }
+    public string Action { get; set; }
+}
 
-//public class UserDeviceLogDto
-//{
-//    public Guid Id { get; set; }
-//    public Guid DeviceId { get; set; }
-//    public Guid ModelId { get; set; }
-//    public string Description { get; set; }
-//    public Guid StatusId { get; set; }
-//    public DateTime AllotedDate { get; set; }
-//    public string Action { get; set; }
-//}
+public class UserDeviceLogDto
+{
+    public Guid Id { get; set; }
+    public Guid DeviceId { get; set; }
+    public Guid ModelId { get; set; }
+    public string Description { get; set; }
+    public Guid StatusId { get; set; }
+    public DateTime AllotedDate { get; set; }
+    public string Action { get; set; }
+}
