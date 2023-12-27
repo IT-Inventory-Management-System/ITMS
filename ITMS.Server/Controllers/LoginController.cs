9be857ee6c909c05981dd2fb488210185fca2724
Harleen Chadha <harleen.chadha@cginfinity.com>
2023-12-27 15:12:51 +0530

    Login API

---
+++ ITMS.Server/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿// Controllers/UserController.cs
+using ITMS.Server.Services;
+using Microsoft.AspNetCore.Mvc;
+using ITMS.Server.DTO;
+
+[ApiController]
+[Route("[controller]")]
+public class LoginController : ControllerBase
+{
+    private readonly LoginService _loginService;
+
+    public LoginController(LoginService loginService)
+    {
+        _loginService = loginService;
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] ITMS.Server.Models.LoginRequestDto loginRequest)
+    {
+        // Call the service for login logic
+        var loginResponse = _loginService.ValidateLogin(loginRequest);
+
+        // Handle the response and return appropriate result
+        if (loginResponse.Success)
+        {
+            return Ok("Login successful");
+        }
+        else
+        {
+            return BadRequest($"Login failed. {loginResponse.Message}");
+        }
+    }
+}
---
+++ ITMS.Server/DTO/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ITMS.Server.DTO
+{
+    public class LoginResponseDto
+    {
+        public bool Success { get; set; }
+        public required string Message { get; set; }
+    }
+}
