bb6a2d2c737bbfd12b9c8f6d0850d1c23e92594f
divyanshrampal28 <divyansh.rampal@cginfinity.com>
2023-12-29 10:31:42 +0530

    add device form started

---
+++ itms.client/src/app/add-asset/add-device-form/add-device-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-device-form',
+  templateUrl: './add-device-form.component.html',
+  styleUrls: ['./add-device-form.component.css']
+})
+export class AddDeviceFormComponent {
+
+}
--- itms.client/src/app/app.module.ts
+++ itms.client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { EmployeeComponent } from './employee/employee.component';
 import { LicenseComponent } from './license/license.component';
 import { HistoryComponent } from './history/history.component';
 import { AddAssetComponent } from './add-asset/add-asset.component';
+import { AddDeviceFormComponent } from './add-asset/add-device-form/add-device-form.component';
 
 
 @NgModule({
@@ -28,6 +29,7 @@ import { AddAssetComponent } from './add-asset/add-asset.component';
     LicenseComponent,
     HistoryComponent,
     AddAssetComponent,
+    AddDeviceFormComponent,
   ],
   imports: [
     BrowserModule,
