b715cf48682269b802bf2db7ed5be8cb40e7b0b9
Bhoomika <bhoomika@cginfinity.com>
2023-12-29 13:18:39 +0530

    controller changes

---
+++ ITMS.Server/Controller/DevicesController.cs
@@ -0,0 +1,45 @@
+﻿//using ITMS.Server.DTO;
+//using ITMS.Server.Services;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace ITMS.Server.Controller
+//{
+//    [ApiController]
+//    [Route("api/categories")]
+//    public class CategoryController : ControllerBase
+//    {
+//        private readonly ICategoryService _categoryService;
+
+//        public CategoryController(ICategoryService categoryService)
+//        {
+//            _categoryService = categoryService;
+//        }
+
+//        [HttpGet("{categoryName}")]
+//        public ActionResult<CategoryDTO> GetCategoryByName(string categoryName)
+//        {
+//            var category = _categoryService.GetCategoryByName(categoryName);
+
+//            if (category == null)
+//            {
+//                return NotFound();
+//            }
+
+//            return Ok(category);
+//        }
+
+//        [HttpGet("range")]
+//        public ActionResult<List<CategoryDTO>> GetCategoriesInRange([FromQuery] string startName, [FromQuery] string endName)
+//        {
+//            var categories = _categoryService.GetCategoriesInRange(startName, endName);
+
+//            if (categories == null || categories.Count == 0)
+//            {
+//                return NotFound();
+//            }
+
+//            return Ok(categories);
+//        }
+//    }
+//}
---
+++ ITMS.Server/Services/DeviceServices.cs
@@ -0,0 +1,52 @@
+﻿//using ITMS.Server.DTO;
+//using ITMS.Server.Models;
+
+//namespace ITMS.Server.Services
+//{
+   
+
+//public interface ICategoryService
+//{
+//    CategoryDTO GetCategoryByName(string categoryName);
+//    List<CategoryDTO> GetCategoriesInRange(string startName, string endName);
+//}
+
+//public class CategoryService : ICategoryService
+//{
+//    private readonly YourDbContext _dbContext; // Replace YourDbContext with your actual DbContext class
+
+//    public CategoryService(YourDbContext dbContext)
+//    {
+//        _dbContext = dbContext;
+//    }
+
+//    public CategoryDTO GetCategoryByName(string categoryName)
+//    {
+//        var categoryEntity = _dbContext.Categories
+//            .Include(c => c.CategoryType)
+//            .FirstOrDefault(c => c.Name == categoryName);
+
+//        return categoryEntity != null ? MapToDTO(categoryEntity) : null;
+//    }
+
+//    public List<CategoryDTO> GetCategoriesInRange(string startName, string endName)
+//    {
+//        var categoriesEntities = _dbContext.Categories
+//            .Include(c => c.CategoryType)
+//            .Where(c => string.Compare(c.Name, startName) >= 0 && string.Compare(c.Name, endName) <= 0)
+//            .ToList();
+
+//        return categoriesEntities.Select(MapToDTO).ToList();
+//    }
+
+//    private CategoryDTO MapToDTO(Category categoryEntity)
+//    {
+//        return new CategoryDTO
+//        {
+//            Id = categoryEntity.Id,
+//            Name = categoryEntity.Name,
+//            TypeName = categoryEntity.CategoryType.TypeName // Assuming navigation property
+//        };
+//    }
+//}
+//    }
