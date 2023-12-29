df03fb3f0a918d341ae599f1ddd2447db90ccf8f
Sanchit Arora <sanchit.arora@cginfinity.com>
2023-12-29 14:34:11 +0530

    category api added

--- ITMS.Server/Services/DashboardService.cs
+++ ITMS.Server/Services/DashboardService.cs
@@ -1,23 +1,23 @@
-﻿using ITMS.Server.Models;
-using System;
+﻿//using ITMS.Server.Models;
+//using System;
 
-namespace ITMS.Server.Services
-{
-    public class DashboardService
-    {
-        private readonly ItinventorySystemContext _context;
+//namespace ITMS.Server.Services
+//{
+//    public class DashboardService
+//    {
+//        private readonly ItinventorySystemContext _context;
 
-        public DashboardService(ItinventorySystemContext context)
-        {
-            _context = context;
-        }
+//        public DashboardService(ItinventorySystemContext context)
+//        {
+//            _context = context;
+//        }
 
-        public IEnumerable<Device> GetInventory()
-        {
-            return _context.Devices.ToList();
-        }
+//        public IEnumerable<Device> GetInventory()
+//        {
+//            return _context.Devices.ToList();
+//        }
 
 
 
-    }
-}
+//    }
+//}
