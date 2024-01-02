81e3d0f5695118390d64ed063a950d9cbc11e4f4
Bhoomika <bhoomika@cginfinity.com>
2024-01-02 11:25:50 +0530

    changes

--- ITMS.Server/Controllers/SoftwareController.cs
+++ ITMS.Server/Controllers/SoftwareController.cs
@@ -1,37 +1,37 @@
-﻿using ITMS.Server.DTO;
-using ITMS.Server.Services;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿//using ITMS.Server.DTO;
+//using ITMS.Server.Services;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace ITMS.Server.Controllers
-{
+//namespace ITMS.Server.Controllers
+//{
    
 
-    // SoftwareController.cs
-    [ApiController]
-    [Route("api/[controller]")]
-    public class SoftwareController : ControllerBase
-    {
-        private readonly SoftwareService _softwareService;
+//    // SoftwareController.cs
+//    [ApiController]
+//    [Route("api/[controller]")]
+//    public class SoftwareController : ControllerBase
+//    {
+//        private readonly SoftwareService _softwareService;
 
-        public SoftwareController(SoftwareService softwareService)
-        {
-            _softwareService = softwareService;
-        }
+//        public SoftwareController(SoftwareService softwareService)
+//        {
+//            _softwareService = softwareService;
+//        }
 
-        [HttpGet]
-        public ActionResult<List<SoftwareDTO>> GetAllSoftware()
-        {
-            var softwareList = _softwareService.GetAllSoftware();
-            return Ok(softwareList);
-        }
+//        [HttpGet]
+//        public ActionResult<List<SoftwareDTO>> GetAllSoftware()
+//        {
+//            var softwareList = _softwareService.GetAllSoftware();
+//            return Ok(softwareList);
+//        }
 
-        [HttpPost]
-        public ActionResult InsertSoftware([FromBody] SoftwareDTO softwareDTO)
-        {
-            _softwareService.InsertSoftware(softwareDTO);
-            return Ok();
-        }
-    }
+//        [HttpPost]
+//        public ActionResult InsertSoftware([FromBody] SoftwareDTO softwareDTO)
+//        {
+//            _softwareService.InsertSoftware(softwareDTO);
+//            return Ok();
+//        }
+//    }
 
-}
+//}
