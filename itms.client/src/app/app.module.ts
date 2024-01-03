8fa7dbbc15d99a4d3891a5094383209e242e0a32
Harleen Chadha <harleen.chadha@cginfinity.com>
2024-01-03 17:32:05 +0530

    Bug Fixes in app.app.module

--- itms.client/src/app/app.module.ts
+++ itms.client/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
+
 import { AvatarModule } from 'ngx-avatars';
 import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -14,7 +14,9 @@ import { LicenseComponent } from './license/license.component';
 import { HistoryComponent } from './history/history.component';
 import { AddAssetComponent } from './add-asset/add-asset.component';
 import { ProfileIconComponent } from './shared/components/profile-icon/profile-icon.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component'; 
 import { NewSidebarComponent } from './shared/components/new-sidebar/new-sidebar.component';
+
 // Use SidebarNewComponent in your code
 
 
