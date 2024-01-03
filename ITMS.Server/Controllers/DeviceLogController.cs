fca5f109a6bb1eac5f8484d36273ebf493a48ace
Sanchit Arora <sanchit.arora@cginfinity.com>
2024-01-03 16:53:04 +0530

    NEW api for devices show

--- ITMS.Server/Controllers/DeviceLogController.cs
+++ ITMS.Server/Controllers/DeviceLogController.cs
@@ -1,56 +1,29 @@
-//// Controllers/DeviceLogController.cs
-//using Microsoft.AspNetCore.Mvc;
+// Controllers/DeviceLogController.cs
+using Microsoft.AspNetCore.Mvc;
 
-//[ApiController]
-//[Route("api/[controller]")]
-//public class DeviceLogController : ControllerBase
-//{
-//    private readonly DeviceLogService _deviceLogService;
+[ApiController]
+[Route("api/[controller]")]
+public class DeviceLogController : ControllerBase
+{
+    private readonly DeviceLogService _deviceLogService;
 
-//    public DeviceLogController(DeviceLogService deviceLogService)
-//    {
-//        _deviceLogService = deviceLogService;
-//    }
+    public DeviceLogController(DeviceLogService deviceLogService)
+    {
+        _deviceLogService = deviceLogService;
+    }
 
-//    [HttpGet("history")]
-//    public IActionResult GetDeviceHistory()
-//    {
-//        try
-//        {
-//            var deviceHistory = _deviceLogService.GetDeviceHistory();
-//            return Ok(deviceHistory);
-//        }
-//        catch (Exception ex)
-//        {
-//            // Log the exception or handle it appropriately
-//            return StatusCode(500, "Internal Server Error");
-//        }
-//    }
-//}
-
-//[ApiController]
-//[Route("api/[controller]")]
-//public class UserController : ControllerBase
-//{
-//    private readonly DeviceLogService _deviceLogService;
-
-//    public UserController(DeviceLogService deviceLogService)
-//    {
-//        _deviceLogService = deviceLogService;
-//    }
-
-//    [HttpGet("{userId}/device-history")]
-//    public IActionResult GetDeviceHistoryForUser(string userId)
-//    {
-//        try
-//        {
-//            var userDeviceHistory = _deviceLogService.GetDeviceHistoryForUser(userId);
-//            return Ok(userDeviceHistory);
-//        }
-//        catch (Exception ex)
-//        {
-//            // Log the exception or handle it appropriately
-//            return StatusCode(500, "Internal Server Error");
-//        }
-//    }
-//}
+    [HttpGet("devices")]
+    public IActionResult GetDeviceHistory()
+    {
+        try
+        {
+            var deviceHistory = _deviceLogService.GetDevices();
+            return Ok(deviceHistory);
+        }
+        catch (Exception ex)
+        {
+            // Log the exception or handle it appropriately
+            return StatusCode(500, "Internal Server Error");
+        }
+    }
+}
--- itms.client/src/app/inventory/assets-category/assest/devices/devices-list/devices-list.component.ts
+++ itms.client/src/app/inventory/assets-category/assest/devices/devices-list/devices-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-devices-list',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./devices-list.component.css']
 })
 export class DevicesListComponent {
+  @Input() Cygid: string = '';
+
+  constructor() {
+    console.log(this.Cygid);
+  }
 
 }
