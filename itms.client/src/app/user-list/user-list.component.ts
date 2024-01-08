f5a0a3223696edf64a610ad09015fe1c49cd653c
vandit6 <87277054+vandit6@users.noreply.github.com>
2024-01-08 14:43:06 +0530

    Update user-list.component.ts

--- itms.client/src/app/user-list/user-list.component.ts
+++ itms.client/src/app/user-list/user-list.component.ts
@@ -1,34 +1,65 @@
-import { Component } from '@angular/core';
+[2:40 PM] Aashna Sharma
+// Import necessary modules and services
+
+import { Component, OnInit } from '@angular/core';
+
 import { DisplayDetailsService } from '../shared/services/display-details.service';
-
+ 
 @Component({
+
   selector: 'app-user-list',
+
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+
+  styleUrls: ['./user-list.component.css'],
+
+  // Add the DisplayDetailsService to the providers array
+
+  providers: [DisplayDetailsService]
+
 })
-export class UserListComponent {
+
+export class UserListComponent implements OnInit {
+
   displayingData: any[] = [];
-  filterInput: string = ''; 
 
-  constructor(private DisplayingDetailsService: DisplayDetailsService) {
-    this.displayingData = [];
+  filterInput: string = '';
+ 
+  constructor(private displayingDetailsService: DisplayDetailsService) {
+
+    // Initialize your class properties here if needed
+
   }
-  
+ 
   ngOnInit(): void {
-    this.showUserListData();
-  }
 
+    this.showUserListData();
+
+  }
+ 
   showUserListData() {
-    this.DisplayingDetailsService.getshowUserListData().subscribe(
+
+    // Use the service to get data
+
+    this.displayingDetailsService.getshowUserListData().subscribe(
+
       (data) => {
+
         this.displayingData = data;
+
         console.log(this.displayingData);
+
       },
+
       (error) => {
+
         console.log(error);
 
       }
-    )
+
+    );
+
   }
 
 }
+
