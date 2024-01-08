caa04be487386eb36bce3ab08b398c32fa01050e
Bhoomika <bhoomika@cginfinity.com>
2024-01-08 11:00:33 +0530

    new changes

--- itms.client/src/app/app.module.ts
+++ itms.client/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { AvatarModule } from 'ngx-avatars';
 import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './dashboard/dashboard.component';
