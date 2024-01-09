e5988e8d2ae4eba3cc529fc74381df8e7ec57e8b
Harleen Chadha <harleen.chadha@cginfinity.com>
2024-01-09 14:27:47 +0530

    Api to fetch software details

--- ITMS.Server/Controllers/SoftwareController.cs
+++ ITMS.Server/Controllers/SoftwareController.cs
@@ -1,37 +1,27 @@
-﻿//using ITMS.Server.DTO;
-//using ITMS.Server.Services;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-
-//namespace ITMS.Server.Controllers
-//{
-   
-
-//    // SoftwareController.cs
-//    [ApiController]
-//    [Route("api/[controller]")]
-//    public class SoftwareController : ControllerBase
-//    {
-//        private readonly SoftwareService _softwareService;
-
-//        public SoftwareController(SoftwareService softwareService)
-//        {
-//            _softwareService = softwareService;
-//        }
-
-//        [HttpGet]
-//        public ActionResult<List<SoftwareDTO>> GetAllSoftware()
-//        {
-//            var softwareList = _softwareService.GetAllSoftware();
-//            return Ok(softwareList);
-//        }
-
-//        [HttpPost]
-//        public ActionResult InsertSoftware([FromBody] SoftwareDTO softwareDTO)
-//        {
-//            _softwareService.InsertSoftware(softwareDTO);
-//            return Ok();
-//        }
-//    }
-
-//}
+﻿using ITMS.Server.Models;
+using ITMS.Server.Services;
+using ITMS.Server.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Elfie.Serialization;
+using static ITMS.Server.Services.GetSoftwareService;
+namespace ITMS.Server.Controllers
+{
+    [ApiController]
+    [Route("api/software")]
+    public class SoftwareController : ControllerBase
+    {
+        private readonly ItinventorySystemContext _context;
+        private readonly IGetSoftwareService _addSoftwareService;
+        public SoftwareController(ItinventorySystemContext context, IGetSoftwareService addSoftwareService)
+        {
+            _context = context;
+            _addSoftwareService = addSoftwareService;
+        }
+        [HttpGet("getSoftware")]
+        public async Task<IEnumerable<GetSoftwareDTO>> listSoftware()
+        {
+            return await _addSoftwareService.listSoftware();
+        }
+    }
+}
--- ITMS.Server/Program.cs
+++ ITMS.Server/Program.cs
@@ -2,6 +2,7 @@ using ITMS.Server.Models;
 using ITMS.Server.Services;
 using Microsoft.EntityFrameworkCore;
 using MiNET.Blocks;
+using static ITMS.Server.Services.GetSoftwareService;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,7 @@ builder.Services.AddDbContext<ItinventorySystemContext>(options =>
 builder.Services.AddScoped<IDeviceService, AddDeviceService>();
 builder.Services.AddScoped<IUserListService, UserListService>();
 builder.Services.AddScoped<IAddAssetService, AddAssetService>();
+builder.Services.AddScoped<IGetSoftwareService, GetSoftwareService>();
 builder.Services.AddScoped<DeviceService>();
 builder.Services.AddScoped<DeviceLogService>();
 
