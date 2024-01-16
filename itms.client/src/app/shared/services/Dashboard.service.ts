5c3f136279917d8abcde285ab63a376011f03c8d
AzureAD\VanditJain <vandit.jain@cginfinity.com>
2024-01-17 00:30:16 +0530

    software api integrated

--- itms.client/src/app/shared/services/Dashboard.service.ts
+++ itms.client/src/app/shared/services/Dashboard.service.ts
@@ -5,13 +5,17 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class EmployeeService {
+export class DashboardService {
 
-  laptopDetails: any;
   private apiUrl = 'https://localhost:44384/api/';
 
   constructor(private http: HttpClient) { }
   GetAccessories(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl + 'Dashboard/GetAccessories/');
+  }
+
+
+  GetSoftwares(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl + 'Dashboard/softwares/');
   }
 }
