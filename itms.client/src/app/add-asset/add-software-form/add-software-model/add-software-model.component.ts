585335fb265ff7918e6fa2d2057cc1a8158cfe99
gargisawaria <gargisawaria1234@gmail.com>
2024-01-18 14:26:30 +0530

    Upload_image working and dynamic webpage component changes

--- itms.client/src/app/add-asset/add-software-form/add-software-model/add-software-model.component.ts
+++ itms.client/src/app/add-asset/add-software-form/add-software-model/add-software-model.component.ts
@@ -6,10 +6,25 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./add-software-model.component.css']
 })
 export class AddSoftwareModelComponent {
-  @Output() radioSelection = new EventEmitter<string>();
+ProfileDP = '';
 
-
-  onRadioButtonSelected(option: string) {
-    this.radioSelection.emit(option);
+ngOnInit(): void {
+  this.ProfileDP = '../../../assets/icons/add_photo_alternate_outlined 1.svg';
   }
-}
+  ImageUpload(event: any): void {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      reader.onload = (e: any) => {
+        
+        this.ProfileDP = e.target.result;
+      };
+
+    }
+  }
+  }
+
+
