fe164e39e5650f4845cf3d14823a0bf62e0a07d0
Naman Malik <naman.malik@cginfinity.com>
2024-01-22 14:54:51 +0530

    1qwerty

--- ITMS.Server/DTO/UserDeviceHistory.cs
+++ ITMS.Server/DTO/UserDeviceHistory.cs
@@ -1,23 +1,23 @@
 ﻿
+using ITMS.Server.Models;
+
 namespace ITMS.Server.DTO
 {
     public class UserDeviceHistory
     {
-        public Guid DeviceId { get; set; } //JUST ONE CHANGE
-        public Guid DeviceLogId { get; set; } //just one change adding the DeviceLogId
-        public string cygid { get; set; }
-        public string Model { get; set; }
+        public Guid? DeviceId { get; set; } 
+        public Guid? DeviceLogId { get; set; } 
+        public string? cygid { get; set; }
+        public string? Model { get; set; }
 
-        public string AssignBy { get; set; }
+        public string? AssignBy { get; set; }
 
-        public DateTime AssignedDate { get; set; }
+        public DateTime? AssignedDate { get; set; }
 
         public string? SubmitedBy { get; set; }
 
         public DateTime? SubmitedByDate { get; set; }
-        public List<CommentDto> Comments { get; set; }
-
-
+        public List<Comment>? Comments { get; set; }
 
     }
 }
--- ITMS.Server/Services/UserDeviceService.cs
+++ ITMS.Server/Services/UserDeviceService.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using ITMS.Server.DTO;
 using ITMS.Server.Models;
 using Microsoft.EntityFrameworkCore;
+using MiNET.Blocks;
 
 // Services/UserDeviceService.cs
 
@@ -52,40 +53,89 @@ public class UserDeviceService
         try
         {
             var devicesWithComments = _dbContext.DevicesLogs
-                .Where(log => log.EmployeeId == id)
-                .Include(e => e.Employee)
+                .Where(e => e.EmployeeId == id)
                 .Include(d => d.Device)
                     .ThenInclude(dm => dm.DeviceModel)
-                .Select(log => new UserDeviceHistory
-                {
-                    cygid = log.Device.Cygid,
-                    DeviceLogId = log.Id,
-                    Model = log.Device.DeviceModel.ModelNo,
-                    DeviceId = log.DeviceId, //one change
-                    AssignBy = _dbContext.Employees
-                        .Where(employee => employee.Id == log.Device.AssignedBy)
-                        .Select(employee => $"{employee.FirstName} {employee.LastName}")
-                        .FirstOrDefault(),
-                    AssignedDate = (DateTime)log.Device.AssignedDate,
-                    Comments = _dbContext.Comments
-                        .Where(comment => comment.DeviceId == log.Device.Id)
-                        .Select(c => new CommentDto
-                        {
-                            Id = c.Id,
-                            Description = c.Description,
-                            CreatedBy = _dbContext.Employees
-                                .Where(employee => employee.Id == c.CreatedBy)
-                                .Select(employee => $"{employee.FirstName} {employee.LastName}")
-                                .FirstOrDefault(),
-                            CreatedAt = c.CreatedAtUtc,
-                            DeviceId = c.DeviceId,
-                            DeviceLogId = c.DeviceLogId
-                        })
-                        .ToList(),
-                })
-                .ToList();
+                    .ToList();
 
-            return devicesWithComments;
+            List<UserDeviceHistory> devices = new List<UserDeviceHistory>();
+            foreach (var logs in devicesWithComments)
+            {
+                UserDeviceHistory singleUser = new UserDeviceHistory();
+                singleUser.DeviceId = logs.Device.Id;
+                singleUser.DeviceLogId = logs.Id;
+                singleUser.cygid = logs.Device.Cygid;
+                singleUser.Model = logs.Device.DeviceModel.ModelNo;
+                singleUser.AssignBy =
+                //logs.Employee.FirstName; 
+                _dbContext.Employees
+                .Where(e => e.Id == logs.AssignedBy)
+                .Select(e => $"{e.FirstName} {e.LastName}")
+                .FirstOrDefault();
+                singleUser.AssignedDate = logs.Device.AssignedDate ?? DateTime.MinValue;
+
+
+                singleUser.SubmitedBy = _dbContext.Employees
+                                     .Where(e => e.Id == logs.RecievedBy)
+                                     .Select(e => $"{e.FirstName} {e.LastName}")
+                                     .FirstOrDefault();
+                singleUser.SubmitedByDate = logs.RecievedDate ?? DateTime.MinValue;
+                singleUser.Comments = _dbContext.Comments.ToList();
+                //            .Where(comment => comment.DeviceId == log.Device.Id)
+                //            .Select(c => new CommentDto
+                //            {
+                //                Id = c.Id,
+                //                Description = c.Description,
+                //                CreatedBy = _dbContext.Employees
+                //                    .Where(employee => employee.Id == c.CreatedBy)
+                //                    .Select(employee => $"{employee.FirstName} {employee.LastName}")
+                //                    .FirstOrDefault(),
+                //                CreatedAt = c.CreatedAtUtc,
+                //                DeviceId = c.DeviceId,
+                //                DeviceLogId = c.DeviceLogId
+                //            })
+                //            .ToList(),
+
+                devices.Add( singleUser );
+            }
+
+            return devices;
+
+            //var devicesWithComments = _dbContext.DevicesLogs
+            //    .Where(log => log.EmployeeId == id)
+            //    .Include(e => e.Employee)
+            //    .Include(d => d.Device)
+            //        .ThenInclude(dm => dm.DeviceModel)
+            //    .Select(log => new UserDeviceHistory
+            //    {
+            //        cygid = log.Device.Cygid,
+            //        DeviceLogId = log.Id,
+            //        Model = log.Device.DeviceModel.ModelNo,
+            //        DeviceId = log.DeviceId, 
+            //        AssignBy = _dbContext.Employees
+            //            .Where(employee => employee.Id == log.Device.AssignedBy)
+            //            .Select(employee => $"{employee.FirstName} {employee.LastName}")
+            //            .FirstOrDefault(),
+            //        AssignedDate = (DateTime)log.Device.AssignedDate,
+            //        Comments = _dbContext.Comments
+            //            .Where(comment => comment.DeviceId == log.Device.Id)
+            //            .Select(c => new CommentDto
+            //            {
+            //                Id = c.Id,
+            //                Description = c.Description,
+            //                CreatedBy = _dbContext.Employees
+            //                    .Where(employee => employee.Id == c.CreatedBy)
+            //                    .Select(employee => $"{employee.FirstName} {employee.LastName}")
+            //                    .FirstOrDefault(),
+            //                CreatedAt = c.CreatedAtUtc,
+            //                DeviceId = c.DeviceId,
+            //                DeviceLogId = c.DeviceLogId
+            //            })
+            //            .ToList(),
+            //    })
+            //    .ToList();
+
+            //return devicesWithComments;
         }
         catch (Exception ex)
         {
