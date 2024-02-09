53ce0e9da387dce60db7bbabbe0d321ce62486e7
gargisawaria <gargisawaria1234@gmail.com>
2024-02-09 17:19:40 +0530

    Worked on Mouse Form

---
+++ ITMS.Server/DTO/AccessoriesDTO.cs
@@ -0,0 +1,38 @@
+﻿namespace ITMS.Server.DTO
+{
+    public class GetBrandDTO
+    {
+        public string brand { get; set; }
+        public bool? iswired { get; set; }
+
+    }
+
+    public class PostMouseModelDTO
+    {
+        public string brand { get; set; }
+        public bool? iswired { get; set; }
+        public Guid categoryId { get; set; }
+        public Guid createdBy { get; set; }
+        public Guid updatedBy { get; set; }
+
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+        public bool isArchived { get; set; }
+
+    }
+
+    public class PostMouseDTO
+    {
+        public List<String> deviceId { get; set; }
+        public int qty { get; set; }
+        public Guid deviceModelId { get; set; }
+        public Guid createdBy { get; set; }
+        public Guid updatedBy { get; set; }
+        public Guid status { get; set; }
+        public Guid locationId { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+        public bool isArchived { get; set; }
+
+    }
+}
