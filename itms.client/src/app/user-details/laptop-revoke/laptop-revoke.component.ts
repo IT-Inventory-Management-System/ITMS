57bc3d9d76da678ed4a179d5baade479ab90b0c9
Jigyasa Chopra <jigyasa.chopra@cginfinity.com>
2024-02-12 16:40:29 +0530

    recieved by

--- itms.client/src/app/user-details/laptop-revoke/laptop-revoke.component.ts
+++ itms.client/src/app/user-details/laptop-revoke/laptop-revoke.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { EmployeeService } from '../../shared/services/Employee.service';
 
 @Component({
   selector: 'app-laptop-revoke',
@@ -12,6 +13,54 @@ export class LaptopRevokeComponent {
   @Input() lastName: any;
   @Input() cgiid: any;
 
+  newComment: string = '';
+  comments: any;
+  loggedUser: any;
+
+  constructor(private commentService: EmployeeService) {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser !== null) {
+      this.loggedUser = JSON.parse(storedUser);
+    }
+  }
+
+  saveCommentAndFetchComments() {
+
+    if (this.newComment) {
+      const commentDto = {
+        description: this.newComment,
+        createdBy: this.loggedUser.id,
+        createdAtUtc: new Date().toISOString(),
+        deviceId: this.laptopDetails.deviceId,
+        deviceLogId: this.laptopDetails.deviceLogId
+      };
+      
+      console.log('Comment DTO:', commentDto);
+
+      this.commentService.addComment(commentDto).subscribe(
+        (response) => {
+
+          console.log(response);
+          this.laptopDetails.comments.unshift(response);
+          console.log(this.laptopDetails.comments);
+          this.newComment = '';
+        },
+        (error) => {
+          console.error('Error adding comment:', error);
+        }
+      );
+      this.newComment = '';
+    }
+
+
+
+    console.log("Device Log Id: ", this.laptopDetails.deviceLogId);
+    console.log("RecievedBy Id: ", this.loggedUser.id);
+
+    
+
+
+  }
 
   openModal(modalId: string) {
     const modelDiv = document.getElementById(modalId);
