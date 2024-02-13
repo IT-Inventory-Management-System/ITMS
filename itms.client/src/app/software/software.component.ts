32b5f811a4a5c2135d5d214ff14dc8961d760396
AzureAD\VanditJain <vandit.jain@cginfinity.com>
2024-02-13 22:59:13 +0530

    table

--- itms.client/src/app/software/software.component.ts
+++ itms.client/src/app/software/software.component.ts
@@ -41,49 +41,18 @@ export class SoftwareComponent implements OnInit {
       suppressMovable: true,
       width: 65
     },
-    {
-      field: "Brand",
-      resizable: false,
-      width: 117,
-      suppressMovable: true
-    },
-    {
-      field: "Operating System", width: 144, resizable: false, suppressMovable: true,
-      cellStyle: (params) => {
-        // Check if the value in "Operating System" column is equal to "Windows 10"
-        if (params.data && params.data['Operating System'] === 'Windows 10') {
-          return {
-            "background-color": "green",
-            "color": "white", // Example color style
-            "padding": "10px",// Example padding style
-
-          };
-        } else {
-          return {
-            "background-color": "black",
-            "color": "white", // Example color style
-            "padding": "10px", // Example padding style
-          };
-        }
-      }
-    },
-    { field: "Model No", width: 164, resizable: false, suppressMovable: true, },
-    { field: "Processor", width: 350, resizable: false, suppressMovable: true, },
-    { field: "Ram (GB)", width: 120, resizable: false, suppressMovable: true, },
-    { field: "Storage", width: 103, resizable: false, suppressMovable: true, },
-    { field: "Serial No", width: 160, resizable: false, suppressMovable: true, },
-    { field: "CYG ID", width: 120, resizable: false, suppressMovable: true, },
-    /*{ field: "# Stock Count", width: 125, resizable: false, suppressMovable: true, },*/
+    { field: "Software Name",  resizable: false,  width: 150,  suppressMovable: true },
+    {field: "Version",  width: 100,  resizable: false, suppressMovable: true },
+    { field: "# Total", width: 90, resizable: false, suppressMovable: true, },
+    { field: "# Assigned", width: 109, resizable: false, suppressMovable: true, },
+    { field: "# Inventory", width: 142, resizable: false, suppressMovable: true, },
     { field: "Date of Purchase", width: 170, resizable: false, suppressMovable: true, },
-    //{ field: "# Total", width: 100, resizable: false, suppressMovable: true, },
-    //{ field: "# Assigned", width: 109, resizable: false, suppressMovable: true, },
-    //{ field: "# Inventory", width: 142, resizable: false, suppressMovable: true, },
-    { field: "Warranty (in Years)", width: 152, resizable: false, suppressMovable: true, },
-    { field: "Assigned To", width: 140, resizable: false, suppressMovable: true, },
+    { field: "Expiry Date", width: 150, resizable: false, suppressMovable: true, },
+    { field: "# Stock Status", width: 125, resizable: false, suppressMovable: true, },
+    { field: "Assigned To", width: 128, resizable: false, suppressMovable: true, },
     { field: "Assigned Date", width: 129, resizable: false, suppressMovable: true, },
-    { field: "Device Status", width: 119, resizable: false, suppressMovable: true, pinned: 'right' },
-    { field: "Action", width: 83, resizable: false, suppressMovable: true, },
-    /* { field: "Stock Status", pinned: 'right', cellStyle: { 'border': 'none' }, width: 122, resizable: false, suppressMovable: true, }*/
+    { field: "Software Status", width: 135, resizable: false, suppressMovable: true, },
+     { field: "Stock Status", pinned: 'right', cellStyle: { 'border': 'none' }, width: 122, resizable: false, suppressMovable: true, }
 
   ];
   filename = 'ExcelSheet.xlsx';
