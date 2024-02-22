93124a5deead957b5c3021f6d449431149a0a901
Naman Malik <naman.malik@cginfinity.com>
2024-02-22 13:55:00 +0530

    dashboard done

--- ITMS.Server/Controllers/DashboardController.cs
+++ ITMS.Server/Controllers/DashboardController.cs
@@ -54,10 +54,10 @@ namespace ITMS.Server.Controllers
             return Ok(os);
         }
 
-        [HttpGet("logs")]
-        public ActionResult<IEnumerable<Logs>> GetLogs()
+        [HttpPost("logs")]
+        public ActionResult<IEnumerable<Logs>> GetLogs([FromBody] Guid country)
         {
-            var history = _dashboardService.GetLogs();
+            var history = _dashboardService.GetLogsForLocation(country, 1);
             return Ok(history);
         }
 
