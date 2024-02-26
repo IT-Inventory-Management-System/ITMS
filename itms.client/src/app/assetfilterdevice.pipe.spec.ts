e138d945564d6fdc5c79f02b7a2bec4793302094
Sonali Chawla <sonali.chawla@msimga.com>
2024-02-27 01:58:58 +0530

    removed client layer

--- itms.client/src/app/assetfilterdevice.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AssetfilterdevicePipe } from './assetfilterdevice.pipe';
-
-describe('AssetfilterdevicePipe', () => {
-  it('create an instance', () => {
-    const pipe = new AssetfilterdevicePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- itms.client/src/app/filter-search-list.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterSearchListPipe } from './filter-search-list.pipe';
-
-describe('FilterSearchListPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterSearchListPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
