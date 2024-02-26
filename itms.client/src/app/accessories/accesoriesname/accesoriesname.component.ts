cf6e5cadd6f04dc8f302f38fff7e222cb8d65e2e
AzureAD\VanditJain <vandit.jain@cginfinity.com>
2024-02-26 16:44:16 +0530

    icons accesories

--- itms.client/src/app/accessories/accesoriesname/accesoriesname.component.ts
+++ itms.client/src/app/accessories/accesoriesname/accesoriesname.component.ts
@@ -17,4 +17,28 @@ export class AccesoriesnameComponent {
     });
   }
 
+
+  getAccessoryIconPath(category: string): string {
+    let iconFileName = '';
+
+    // Determine the icon file name based on the category
+    switch (category) {
+      case 'Mouse':
+        iconFileName = 'Mouse.svg';
+        break;
+      case 'Keyboard':
+        iconFileName = 'Keyboard.svg';
+        break;
+      // Add more cases for other categories as needed
+      default:
+        // Default icon if category doesn't match any specific case
+        iconFileName = 'Ellipse.svg';
+        break;
+    }
+
+    // Return the full path to the icon file
+    return `../../../assets/icons/Accessories/${iconFileName}`;
+  }
+
+
 }
