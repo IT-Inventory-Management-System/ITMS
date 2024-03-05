264e975242db2534e51908ab34a79a7a9b8279a1
vandit6 <87277054+vandit6@users.noreply.github.com>
2024-03-06 00:21:39 +0530

    Update Employee.service.ts

--- itms.client/src/app/shared/services/Employee.service.ts
+++ itms.client/src/app/shared/services/Employee.service.ts
@@ -10,7 +10,7 @@ export class EmployeeService {
   laptopDetails: any;
   softwareDetails: any;
   accessoriesDetails: any;
-  private apiUrl = 'https://localhost:7283/api/';
+  private apiUrl = 'https://itimsbackend.somee.com/api/';
 
   constructor(private http: HttpClient) { }
   //getDevices(id: any): Observable<any[]> {
