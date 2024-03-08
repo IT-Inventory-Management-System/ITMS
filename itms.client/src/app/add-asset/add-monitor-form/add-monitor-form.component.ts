e418cbd457f11b4ff0702d1ed613ee47965fe794
gargisawaria <gargisawaria1234@gmail.com>
2024-03-08 18:02:11 +0530

    add_monitor_UI

--- itms.client/src/app/add-asset/add-monitor-form/add-monitor-form.component.ts
+++ itms.client/src/app/add-asset/add-monitor-form/add-monitor-form.component.ts
@@ -9,7 +9,10 @@ export class AddMonitorFormComponent {
   currentStep: number = 1;
   ifChecked: boolean = false;
   ifCheck: boolean = false;
+  showDeviceDetailsForm: boolean=false;
   iCheck: boolean = false;
+  selectedStorage: string | null = null;
+  counterValue = 0;
 
   toggleDeviceDetailsForm() {
     this.ifChecked = !this.ifChecked;
@@ -21,4 +24,55 @@ export class AddMonitorFormComponent {
   toggleDevice() {
     this.iCheck = !this.iCheck;
   }
+  toggleDetails() {
+    this.showDeviceDetailsForm = !this.showDeviceDetailsForm;
+  }
+  onFormSubmitted() {
+    this.showDeviceDetailsForm = false;
+  }
+  selectStorage(value: string) {
+    this.selectedStorage = value;
+    //this.deviceForm.get('storage')?.setValue(value);
+    //this.hideErrorMessage();
+
+  }
+
+  selectOtherStorage(event: any) {
+    if (this.selectedStorage == null) {
+      //this.deviceForm.get('storage')?.setValue(event.target.value);
+      //this.hideErrorMessage();
+    }
+  
+  }
+
+
+  deselectAllButtons() {
+    this.selectedStorage = ''; // Deselect all buttons when input field is clicked
+  }
+  decrement() {
+    if (this.counterValue > 0) {
+      this.counterValue--;
+
+    };
+  }
+    increment() {
+      this.counterValue++;
+   
+      };
+    
+  next() {
+
+
+      this.currentStep++;
+    
+
+  }
+
+
+  previous() {
+    this.currentStep--;
+  }
+  
+
+  
 }
