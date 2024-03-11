4ef6626b7b5e5d3b6c6520e683933fa7b597b8e8
divyanshrampal28 <divyansh.rampal@cginfinity.com>
2024-03-11 10:17:25 +0530

    accessory shared component

---
+++ itms.client/src/app/shared/components/add-accessory-shared/add-accessory-brand-form/add-accessory-brand-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-accessory-brand-form',
+  templateUrl: './add-accessory-brand-form.component.html',
+  styleUrls: ['./add-accessory-brand-form.component.css']
+})
+export class AddAccessoryBrandFormComponent {
+
+}
