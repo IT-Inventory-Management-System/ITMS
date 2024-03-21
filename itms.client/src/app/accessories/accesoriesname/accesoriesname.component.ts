a0da90a5b197b68e5eedc8d04764af3b2c0c6fb6
AzureAD\VanditJain <vandit.jain@cginfinity.com>
2024-03-21 11:58:26 +0530

    commit

--- itms.client/src/app/accessories/accesoriesname/accesoriesname.component.ts
+++ itms.client/src/app/accessories/accesoriesname/accesoriesname.component.ts
@@ -56,6 +56,10 @@ export class AccesoriesnameComponent {
         iconFileName = 'server-blue.svg';
         break;
 
+      case 'android cables':
+        iconFileName = 'android-cables-blue.svg';
+        break;
+
       case 'apple thunderbolt(lan)':
         iconFileName = 'apple-thunderboltlan-blue.svg';
         break;
