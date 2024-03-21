2d614f770e3f7a55d6094b32c7b1c9112255f228
Harleen Chadha <harleen.chadha@cginfinity.com>
2024-03-21 11:37:41 +0530

    backend change

--- ITMS.Server/DTO/AccessoriesDTO.cs
+++ ITMS.Server/DTO/AccessoriesDTO.cs
@@ -34,7 +34,7 @@
         public DateTime updatedAt { get; set; }
         public bool isArchived { get; set; }
         public DateTime purchaseddate { get; set; }
-        public DateTime warrantydate { get; set; }
+        public DateTime? warrantydate { get; set; }
 
     }
     public class getCGIDTO
@@ -126,7 +126,7 @@
         public DateTime updatedAt { get; set; }
         public bool isArchived { get; set; }
         public DateTime purchaseddate { get; set; }
-        public DateTime warrantydate { get; set; }
+        public DateTime? warrantydate { get; set; }
 
     }
 
